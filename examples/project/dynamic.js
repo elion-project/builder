6f52d542691d075394508fc223186d8c60855a04
Yeskiy <eqwer24@outlook.com>
2022-10-16 19:02:48 +0300

    feat: add ESM support of dynamic import

---
+++ examples/project/dynamic.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+export default function helloWorld() {
+    console.log(chalk.bgBlue("Hello world from dynamic imported file"));
+}
--- examples/project/index.js
+++ examples/project/index.js
@@ -1,6 +1,7 @@
 import chalk, { modifiers } from "chalk";
 
 (async () => {
+    (await import("./dynamic")).default();
     await console.log("Hello World");
     console.log("chalk =>", chalk);
     console.log("chalk modifiers =>", modifiers);
