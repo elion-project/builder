1b3853727dcaa002405a66d83efc507fb5d64d85
Yeskiy <eqwer24@outlook.com>
2023-01-13 03:58:34 +0200

    fix: Deny class and function name changing

--- examples/project/index.js
+++ examples/project/index.js
@@ -1,8 +1,10 @@
 import chalk, { modifiers } from "chalk";
 
+class SomeClass {}
 (async () => {
     (await import("./dynamic")).default();
     await console.log("Hello World");
+    console.log("SomeClass class name", SomeClass.name);
     console.log("chalk =>", chalk);
     console.log("chalk modifiers =>", modifiers);
     console.log(chalk.greenBright("Here is cool color used"));
