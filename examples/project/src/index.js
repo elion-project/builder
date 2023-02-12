d0f4537ca4eaceb84a88495c9171698cfd3818b4
Yeskiy <eqwer24@outlook.com>
2023-02-13 00:27:27 +0200

    fix: Resolved some code smells

--- examples/project/src/index.js
+++ examples/project/src/index.js
@@ -3,7 +3,7 @@ import chalk, { modifiers } from "chalk";
 class SomeClass {}
 (async () => {
     (await import("../dynamic")).default();
-    await console.log("Hello World");
+    console.log("Hello World");
     console.log("SomeClass class name", SomeClass.name);
     console.log("chalk =>", chalk);
     console.log("chalk modifiers =>", modifiers);
